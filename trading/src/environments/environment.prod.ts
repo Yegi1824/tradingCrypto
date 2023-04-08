3c2dfd18ee6588293558023caf79898fc18fa77f
Yehor <egorkaishchenko@gmail.com>
2023-04-08 15:58:26 +0300

    initial commit

---
+++ trading/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'cryptotrade-a5ee6',
+    appId: '1:141890926260:web:3530956aa53bf9762f09e1',
+    storageBucket: 'cryptotrade-a5ee6.appspot.com',
+    apiKey: 'AIzaSyBOXizKzEDHVDc0fYRAwQC3NCe07WYyW3Y',
+    authDomain: 'cryptotrade-a5ee6.firebaseapp.com',
+    messagingSenderId: '141890926260',
+    measurementId: 'G-KP5H40GDBQ',
+  },
+  production: true
+};
---
+++ trading/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'cryptotrade-a5ee6',
+    appId: '1:141890926260:web:3530956aa53bf9762f09e1',
+    storageBucket: 'cryptotrade-a5ee6.appspot.com',
+    apiKey: 'AIzaSyBOXizKzEDHVDc0fYRAwQC3NCe07WYyW3Y',
+    authDomain: 'cryptotrade-a5ee6.firebaseapp.com',
+    messagingSenderId: '141890926260',
+    measurementId: 'G-KP5H40GDBQ',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
